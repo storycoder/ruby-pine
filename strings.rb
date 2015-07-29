594d9ad213068f7db746efd3c786c7100d432e61
Storycoder <joseph.margolis@gmail.com>
2015-07-29 13:17:24 -0400

    first commit

---
+++ strings.rb
@@ -0,0 +1,17 @@
+puts "hello world"
+puts ''
+puts 'Good-bye.'
+puts 'I like' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+puts "blink " * 4
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+puts 'you\'re swell'
+puts 'You\'re swell'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
