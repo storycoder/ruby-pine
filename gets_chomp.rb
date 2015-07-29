751010bc5784c3811efdbf7dcc2e202bbfa1c346
Storycoder <joseph.margolis@gmail.com>
2015-07-29 14:02:46 -0400

    Chapter 5 work

---
+++ gets_chomp.rb
@@ -0,0 +1,11 @@
+puts "with gets"
+puts "Hello there, what's your name?"
+name = gets
+puts "Your name is " + name + "? What a lovely name!"
+puts "Pleased to meet you, " + name + ". :)"
+puts ''
+puts "with gets.chomp"
+puts "Hello there, what's your name?"
+name = gets.chomp
+puts "Your name is " + name + "? What a lovely name!"
+puts "Pleased to meet you, " + name + ". :)"
