22714f3f919d1e92f3f97cd339f8572b6c613a28
Storycoder <joseph.margolis@gmail.com>
2015-07-29 14:11:53 -0400

    Chapter 5 updated

---
+++ fave_number.rb
@@ -0,0 +1,3 @@
+puts "Hi there. Would you mind telling me your favorite number?"
+number = gets.chomp.to_i
+puts "That's good choice and I mean no disrespect, but I personally believe #{number + 1}, being bigger, is the better number."
---
+++ full_name.rb
@@ -0,0 +1,7 @@
+puts "What's your first name?"
+first = gets.chomp
+puts "What's your middle name?"
+middle = gets.chomp
+puts "What's your last name?"
+last = gets.chomp
+puts "Hi #{first} #{middle} #{last}! I'm Ruby. It's my absolute pleasure to meet you!"
